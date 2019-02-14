624aa29bf7381dc01789179aa6c481f7baae50cb
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2019-02-14 17:34:58 +0100

    Added init function to roomFinder
    The init function sets the configuration of the module

--- roomfinder.js
+++ roomfinder.js
@@ -8,7 +8,8 @@
 const _ = require('lodash')
 const Promise = require('promise')
 const Intranet = require('intra-api')
-const Intra = new Intranet('auth-c30d0b55a4cc2137dd74941ed92701f5eaffd87b', 'julien.philippon@epitech.eu');
+var Intra = undefined
+var config = undefined
 
 function parseRoomCode(code) {
   segments = code.split('/')
@@ -20,33 +21,33 @@ function parseRoomCode(code) {
   }
 }
 
+function init (token, login, city, rooms) {
+  config = {
+    token: token,
+    login: login,
+    city: city,
+    rooms: rooms
+  }
+  Intra = new Intranet(token, login)
+}
 
-function roomFinder (date) {
+function find (date) {
   return new Promise(function (resolve, reject) {
+    if (config === undefined) {
+      reject("Call .init() first")
+    }
     Intra
     .planning
     .get({startDate: date, endDate: date})
     .then((res) => {
       // Initialize rooms array
-      var roomsList = {
-        "101-George-Boole": 0,
-        "101a-George": "101-George-Boole",
-        "101b-Boole": "101-George-Boole",
-        "102-Ada-Lovelace": 0,
-        "102a-Ada": "102-Ada-Lovelace",
-        "102b-Lovelace": "102-Ada-Lovelace",
-        "103-Hollerith": 0,
-        "104-Rosen-Bull": 0,
-        "105-Heafield": 0,
-        "106-Kleinrock": 0,
-        "107-Tomlinson": 0
-      }
+      var roomsList = _.clone(config.rooms)
       // Count occupations of each room
       for (activity of res) {
         // Verify if the activy has a room, if it is in the good building
         if (!_.isNull(activity.room) && !_.isUndefined(activity.room.code)) {
           room = parseRoomCode(activity.room.code)
-          if (room.country == 'FR' && room.city == 'LYN' && room.building == 'Campus-Lyon-Jean-Mace') {
+          if (room.city == config.city) {
             // Redirect partial rooms to full rooms
             roomName = room.room
             if (roomName in roomsList && _.isString(roomsList[roomName])) {
@@ -75,4 +76,4 @@ function roomFinder (date) {
   })
 }
 
-module.exports = roomFinder
+module.exports = {find: find, init: init}
