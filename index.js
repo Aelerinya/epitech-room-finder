d2d326f526986df7702f83a31d01467c426feff7
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2019-02-14 12:21:40 +0100

    Now the server send the list of the best rooms of the day

--- index.js
+++ index.js
@@ -6,7 +6,18 @@
 */
 
 const express = require('express')
+const roomFinder = require('./roomfinder')
 const app = express()
+
+app.get('/', function (req, res) {
+  const myDate = new Date().toISOString().slice(0, 10)
+  roomFinder(myDate).then(function (rooms) {
+    console.log(`A request for the best room has been fulfilled ! ${rooms}`)
+    res.json(rooms)
+  }).catch(function (err) {
+    res.send('An error occured.')
+  })
+})
 
 app.use(function (req, res) {
   res.statusCode = 404
